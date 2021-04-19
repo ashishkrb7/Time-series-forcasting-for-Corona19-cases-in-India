d02d209c4a5681e1b10f618738266a6bee7293ed
Ashish Kumar <60195105+ashishkrb7@users.noreply.github.com>
2021-04-19 06:39:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import plotly.express as px
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = "Corona update"
 
 def model():
     data_url = "https://api.covid19india.org/data.json"
